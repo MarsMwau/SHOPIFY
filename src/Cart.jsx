389bca561ecbb54914aa3ed75ca66ae748ae8355
Humphrey <humphrey.muhuri@student.moringaschool.com>
2023-05-04 09:37:46 +0300

    in progress

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -1,9 +1,30 @@
-const Cart = () => {
-    return ( 
-        <div className="Cart">
-            
-        </div>
-     );
+import React, { useState } from 'react';
+
+function AddToCart() {
+  const [quantity, setQuantity] = useState(1);
+
+  const handleAdd = () => {
+    setQuantity(quantity + 1);
+  };
+
+  const handleSubtract = () => {
+    if (quantity > 1) {
+      setQuantity(quantity - 1);
+    }
+  };
+
+  const handleAddToCart = () => {
+    // add logic for adding item to cart
+  };
+
+  return (
+    <div>
+      <button onClick={handleSubtract}>-</button>
+      <span>{quantity}</span>
+      <button onClick={handleAdd}>+</button>
+      <button onClick={handleAddToCart}>Add to Cart</button>
+    </div>
+  );
 }
- 
-export default Cart;
+
+export default AddToCart;
