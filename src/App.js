562c37bb04e6c321ca8cdc26b113fb2756e90f84
William Mtemi Mponzi <willbrian009@gmail.com>
2023-05-04 16:10:25 +0300

    Navbar update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React from "react";
 import './App.css';
 import NavBar from './NavBar';
-import Home from "./Home"
-import ProductList from "./ProductList";
-import {Switch, Route} from "react-router-dom"
+import Product from "./ProductList";
 function App(){
     return(
         <div>
-            <NavBar />
-            <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/products" component={ProductList} />
-            </Switch>
+    <NavBar />
+    <Product />
+    
         </div>
     );
 }
+
 export default App;
