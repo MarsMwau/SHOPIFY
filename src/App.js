aa3a785f4c6e9adb178f00bd871594fb1073f2fa
William Mtemi Mponzi <willbrian009@gmail.com>
2023-05-04 14:56:14 +0300

    app.js code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
-import './App.css'
-import NavBar from "./NavBar";
-
+import './App.css';
+import NavBar from './NavBar';
+import Home from "./Home"
+import ProductList from "./ProductList";
+import {Switch, Route} from "react-router-dom"
 function App(){
     return(
         <div>
-    <NavBar />
-    <Product />
-    
+            <NavBar />
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/products" component={ProductList} />
+            </Switch>
         </div>
     );
 }
-
 export default App;
